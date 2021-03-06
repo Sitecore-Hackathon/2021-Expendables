1904e3af5237d98c515ce48cd5d5e5607677accf
Arvind Gehlot <agehlot@horizontal.com>
2021-03-06 14:05:32 +0530

    Added gutter feature

---
+++ src/Feature/Gutter/Models/ItemModel.cs
@@ -0,0 +1,14 @@
+﻿using RealtimeNotifier.Foundation.SignalR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace RealtimeNotifier.Feature.Gutter.Models
+{
+    public class ItemModel : BaseModel
+    {
+        
+    }
+}
--- src/Foundation/SignalR/website/NotificationHub.cs
+++ src/Foundation/SignalR/website/NotificationHub.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNet.SignalR;
-using RealtimeNotifier.Foundation.SignalR.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +8,6 @@ namespace RealtimeNotifier.Foundation.SignalR
 {
     public class NotificationHub : Hub
     {
-        public void Notify(Notification notification)
-        {
-            Clients.All.notify(notification);
-        }
+
     }
 }
