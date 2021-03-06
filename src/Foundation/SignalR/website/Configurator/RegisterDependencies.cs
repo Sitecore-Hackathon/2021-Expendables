0ca5f39b9f7bece5c1b6cf6589cd60ee45842d06
Jatin Prajapati <jprajapati@horizontal.com>
2021-03-06 09:52:44 +0530

    Implemented DI based service to send notifications.

---
+++ src/Foundation/SignalR/website/Configurator/RegisterDependencies.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RealtimeNotifier.Foundation.SignalR.Services;
+using Sitecore.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealtimeNotifier.Foundation.SignalR.Configurator
+{
+    public class RegisterDependencies : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection services)
+        {
+            services.AddSingleton(typeof(ISignalRService), typeof(SignalService));
+        }
+    }
+}
---
+++ src/Foundation/SignalR/website/Services/SignalService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using RealtimeNotifier.Foundation.SignalR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealtimeNotifier.Foundation.SignalR.Services
+{
+    public class SignalService : ISignalRService
+    {
+        public void Signal<THub>(BaseModel tModel) where THub : IHub
+        {
+            var clients = GlobalHost.ConnectionManager.GetHubContext<THub>().Clients;
+            clients.All.notify(tModel);
+        }
+    }
+}
