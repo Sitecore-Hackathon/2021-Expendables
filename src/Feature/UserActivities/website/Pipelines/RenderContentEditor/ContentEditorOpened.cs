b0bfecaf3246d6fc020e4c02b0000e4fb6399ebe
Varun Shringarpure <Varun.Shringarpure-v@adityabirlacapital.com>
2021-03-06 20:06:16 +0530

    Bug fix and Namespace fix

--- src/Feature/UserActivities/website/Pipelines/RenderContentEditor/ContentEditorOpened.cs
+++ src/Feature/UserActivities/website/Pipelines/RenderContentEditor/ContentEditorOpened.cs
@@ -1,18 +1,29 @@
-﻿using RealtimeNotifier.Feature.UserActivities.Model;
+﻿using System;
+using System.Web;
+using RealtimeNotifier.Feature.UserActivities.Model;
 using RealtimeNotifier.Foundation.SignalR.Models;
 using RealtimeNotifier.Foundation.SignalR.Services;
 using Sitecore.DependencyInjection;
 using Sitecore.Shell.Applications.ContentEditor.Pipelines.RenderContentEditor;
-using System;
 
-namespace RealtimeNotifier.Feature.ContentEditorOpened.Pipelines.RenderContentEditor
+namespace RealtimeNotifier.Feature.UserActivities.Pipelines.RenderContentEditor
 {
     public class ContentEditorOpened
     {
         public void Process(RenderContentEditorArgs args)
         {
+
+            if (HttpContext.Current.Request.Url.ToString().ToLowerInvariant().Contains("/sitecore/shell/applications/workbox/commenteditor.aspx"))
+            {
+                return;
+            }
             var signalService = ServiceLocator.ServiceProvider.GetService(typeof(ISignalRService)) as ISignalRService;
-            signalService.Signal(new UserActivityNotification() { Message = $"Opened the content editor.", UserName = Sitecore.Context.User.Profile.UserName, NotificationType = NotificationType.ContentEditorOpened, DateTime = DateTime.Now.ToString() });
+            if (signalService != null)
+                signalService.Signal(new UserActivityNotification()
+                {
+                    Message = $"Opened the content editor.", UserName = Sitecore.Context.User.Profile.UserName,
+                    NotificationType = NotificationType.ContentEditorOpened, DateTime = DateTime.Now.ToString()
+                });
         }
     }
 }
