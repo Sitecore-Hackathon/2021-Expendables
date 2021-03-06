04b2dd26f469a28aeada144ebea8d59d056d91d5
Jatin Prajapati <jprajapati@horizontal.com>
2021-03-06 19:59:22 +0530

    Implemented notification dropdown with number badge on bell icon.

--- src/Feature/UserActivities/website/Pipelines/RenderContentEditor/ContentEditorOpened.cs
+++ src/Feature/UserActivities/website/Pipelines/RenderContentEditor/ContentEditorOpened.cs
@@ -4,13 +4,18 @@ using RealtimeNotifier.Foundation.SignalR.Services;
 using Sitecore.DependencyInjection;
 using Sitecore.Shell.Applications.ContentEditor.Pipelines.RenderContentEditor;
 using System;
+using System.Web;
 
-namespace RealtimeNotifier.Feature.ContentEditorOpened.Pipelines.RenderContentEditor
+namespace RealtimeNotifier.Feature.UserActivities.Pipelines.RenderContentEditor
 {
     public class ContentEditorOpened
     {
         public void Process(RenderContentEditorArgs args)
         {
+            if (HttpContext.Current.Request.Url.ToString().ToLowerInvariant().Contains("/sitecore/shell/applications/workbox/commenteditor.aspx"))
+            {
+                return;
+            }
             var signalService = ServiceLocator.ServiceProvider.GetService(typeof(ISignalRService)) as ISignalRService;
             signalService.Signal(new UserActivityNotification() { Message = $"Opened the content editor.", UserName = Sitecore.Context.User.Profile.UserName, NotificationType = NotificationType.ContentEditorOpened, DateTime = DateTime.Now.ToString() });
         }
